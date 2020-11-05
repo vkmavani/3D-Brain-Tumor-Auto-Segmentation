f721456adc2e59536162ed4942e24ad566022565
Vatsal Mavani <67254410+vkmavani@users.noreply.github.com>
2020-11-05 12:20:22 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,124 @@
+import cv2
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+
+from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data.sampler import SubsetRandomSampler 
+
+
+# ------ Get Label Image ------#
+
+def get_labeled_image(image, label, is_categorical=False):
+    if not is_categorical:
+        label = to_categorical(label, num_classes=4).astype(np.uint8)
+
+    image = cv2.normalize(image[:, :, :, 0], None, alpha=0, beta=255,
+                          norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F).astype(
+        np.uint8)
+
+    labeled_image = np.zeros_like(label[:, :, :, 1:])
+
+    # remove tumor part from image
+    labeled_image[:, :, :, 0] = image * (label[:, :, :, 0])
+    labeled_image[:, :, :, 1] = image * (label[:, :, :, 0])
+    labeled_image[:, :, :, 2] = image * (label[:, :, :, 0])
+
+    # color labels
+    labeled_image += label[:, :, :, 1:] * 255
+    return labeled_image
+
+
+
+# ------ DataGenerator ------#
+
+def DataGenerator(X, y, batch_size=6, valid_size=0.3):
+    dataset = TensorDataset(X, y)
+    len_dataset = len(dataset)
+
+    indices = list(range(len_dataset))
+    np.random.shuffle(indices)
+    split = int(np.floor(valid_size * len_dataset))
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = DataLoader(dataset, batch_size = batch_size, sampler=train_sampler)
+    valid_loader = DataLoader(dataset, batch_size = batch_size, sampler=valid_sampler)
+
+    return train_loader, valid_loader
+
+
+# ------ For Visualization ------#
+
+def plot_image_grid(image):
+    data_all = []
+
+    data_all.append(image)
+
+    fig, ax = plt.subplots(3, 6, figsize=[16, 9])
+
+    # coronal plane
+    coronal = np.transpose(data_all, [1, 3, 2, 4, 0])
+    coronal = np.rot90(coronal, 1)
+
+    # transversal plane
+    transversal = np.transpose(data_all, [2, 1, 3, 4, 0])
+    transversal = np.rot90(transversal, 2)
+
+    # sagittal plane
+    sagittal = np.transpose(data_all, [2, 3, 1, 4, 0])
+    sagittal = np.rot90(sagittal, 1)
+
+    for i in range(6):
+        n = np.random.randint(coronal.shape[2])
+        ax[0][i].imshow(np.squeeze(coronal[:, :, n, :]))
+        ax[0][i].set_xticks([])
+        ax[0][i].set_yticks([])
+        if i == 0:
+            ax[0][i].set_ylabel('Coronal', fontsize=15)
+
+    for i in range(6):
+        n = np.random.randint(transversal.shape[2])
+        ax[1][i].imshow(np.squeeze(transversal[:, :, n, :]))
+        ax[1][i].set_xticks([])
+        ax[1][i].set_yticks([])
+        if i == 0:
+            ax[1][i].set_ylabel('Transversal', fontsize=15)
+
+    for i in range(6):
+        n = np.random.randint(sagittal.shape[2])
+        ax[2][i].imshow(np.squeeze(sagittal[:, :, n, :]))
+        ax[2][i].set_xticks([])
+        ax[2][i].set_yticks([])
+        if i == 0:
+            ax[2][i].set_ylabel('Sagittal', fontsize=15)
+
+    fig.subplots_adjust(wspace=0, hspace=0)
+
+
+def visualize_data_gif(data_):
+    images = []
+    for i in range(data_.shape[0]):
+        x = data_[min(i, data_.shape[0] - 1), :, :]
+        y = data_[:, min(i, data_.shape[1] - 1), :]
+        z = data_[:, :, min(i, data_.shape[2] - 1)]
+        img = np.concatenate((x, y, z), axis=1)
+        images.append(img)
+    imageio.mimsave("/tmp/gif.gif", images, duration=0.01)
+    return Image(filename="/tmp/gif.gif", format='png')
+
+
+def visualize_patch(X, y):
+    fig, ax = plt.subplots(1, 2, figsize=[10, 5], squeeze=False)
+
+    ax[0][0].imshow(X[:, :, 0], cmap='Greys_r')
+    ax[0][0].set_yticks([])
+    ax[0][0].set_xticks([])
+    ax[0][1].imshow(y[:, :, 0], cmap='Greys_r')
+    ax[0][1].set_xticks([])
+    ax[0][1].set_yticks([])
+
+    fig.subplots_adjust(wspace=0, hspace=0)
